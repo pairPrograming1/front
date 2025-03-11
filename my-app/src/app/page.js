5695ccef5f19ed3e7489aedaf99216bd01d879cd
PHRCOOK <pabloelleproso@gmail.com>
2025-03-11 09:10:13 -0300

    modificaciones en login

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,22 @@
-import Login from "./login";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleLoginClick = () => {
+    router.push("/login");
+  };
+
   return (
-    <div>
-      <Login />
+    <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl mb-4">Home</h1>
+      <button
+        onClick={handleLoginClick}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Iniciar sesion
+      </button>
     </div>
   );
 }
