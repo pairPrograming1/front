b8465be6349c572a0d04abcbb94a568c5650bff5
PHRCOOK <pabloelleproso@gmail.com>
2025-03-11 09:19:44 -0300

    creacion de componente register

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -8,14 +8,24 @@ export default function Home() {
     router.push("/login");
   };
 
+  const handleRegisterClick = () => {
+    router.push("/register");
+  };
+
   return (
     <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl mb-4">Home</h1>
       <button
         onClick={handleLoginClick}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
       >
         Iniciar sesion
+      </button>
+      <button
+        onClick={handleRegisterClick}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Registrarse
       </button>
     </div>
   );
