5b56a0cd0fcaff71128cdb101b13b3656b8f5770
PHRCOOK <pabloelleproso@gmail.com>
2025-03-30 13:52:16 -0300

    correcion de bugs en layout

--- Colours/src/app/layout.js
+++ Colours/src/app/layout.js
@@ -17,8 +17,11 @@ const geistMono = Geist_Mono({
 
 export default function RootLayout({ children }) {
   const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
+  const [isMounted, setIsMounted] = React.useState(false);
 
   React.useEffect(() => {
+    setIsMounted(true); // Indica que el componente ya está montado.
+
     if (typeof window !== "undefined") {
       const handleMouseMove = (event) => {
         setMousePosition({ x: event.clientX, y: event.clientY });
@@ -32,22 +35,27 @@ export default function RootLayout({ children }) {
   }, []);
 
   return (
-    <Auth0Provider
-      domain="pabloelleproso.us.auth0.com" // Usar el dominio proporcionado
-      clientId="LQZvjXTatV5t7VzdekZ7sSYZYdoAyndN" // Usar el clientId proporcionado
-      redirectUri={window.location.origin}
-    >
-      <div
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center relative`}
-        style={{
-          background:
-            typeof window !== "undefined"
-              ? `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(25, 119, 97, 0.85), transparent 80%), rgb(5, 34, 62)`
-              : "rgb(5, 34, 62)",
-        }}
-      >
-        {children}
-      </div>
-    </Auth0Provider>
+    <html lang="en">
+      <body>
+        <Auth0Provider
+          domain="pabloelleproso.us.auth0.com" // Usar el dominio proporcionado
+          clientId="LQZvjXTatV5t7VzdekZ7sSYZYdoAyndN" // Usar el clientId proporcionado
+          redirectUri={
+            typeof window !== "undefined" ? window.location.origin : ""
+          }
+        >
+          <div
+            className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center relative`}
+            style={{
+              background: isMounted
+                ? `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(25, 119, 97, 0.85), transparent 80%), rgb(5, 34, 62)`
+                : "rgb(5, 34, 62)",
+            }}
+          >
+            {children}
+          </div>
+        </Auth0Provider>
+      </body>
+    </html>
   );
 }
