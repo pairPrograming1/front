9a548bdfb55063b0d029a95b1d84713f7acc5539
estebanaleart <esteban.aleart@gmail.com>
2025-04-02 22:56:05 -0300

    icons

---
+++ Colours/src/app/components/icons/EyeClosed.jsx
@@ -0,0 +1,12 @@
+const EyeClosed = () => {
+    return (
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="12" r="10" stroke="#FFFFFF" strokeWidth="1.5" />
+        <path d="M7 12L17 12" stroke="#BF8D6B" strokeWidth="1.5" strokeLinecap="round" />
+      </svg>
+    )
+  }
+  
+  export default EyeClosed
+  
+  
---
+++ Colours/src/app/components/icons/EyeOpen.jsx
@@ -0,0 +1,15 @@
+const EyeOpen = () => {
+    return (
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="12" r="10" stroke="#FFFFFF" strokeWidth="1.5" />
+        <path
+          d="M12 6.5C7.5 6.5 3.5 9.5 2 12C3.5 14.5 7.5 17.5 12 17.5C16.5 17.5 20.5 14.5 22 12C20.5 9.5 16.5 6.5 12 6.5ZM12 15.5C10.1 15.5 8.5 13.9 8.5 12C8.5 10.1 10.1 8.5 12 8.5C13.9 8.5 15.5 10.1 15.5 12C15.5 13.9 13.9 15.5 12 15.5ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10Z"
+          fill="#BF8D6B"
+        />
+      </svg>
+    )
+  }
+  
+  export default EyeOpen
+
+  
