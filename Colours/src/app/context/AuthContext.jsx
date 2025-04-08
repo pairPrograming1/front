7c986b89df288443a63edd9e9372e7805c6ee233
PHRCOOK <pabloelleproso@gmail.com>
2025-04-07 21:41:33 -0300

    correcion de bugs en authcontext

--- Colours/src/app/context/AuthContext.jsx
+++ Colours/src/app/context/AuthContext.jsx
@@ -5,17 +5,22 @@ export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [authData, setAuthData] = useState(() => {
-    // Cargar el estado inicial desde localStorage
-    const storedAuthData = localStorage.getItem("authData");
-    return storedAuthData ? JSON.parse(storedAuthData) : null;
+    // Verificar si estamos en el cliente antes de acceder a localStorage
+    if (typeof window !== "undefined") {
+      const storedAuthData = localStorage.getItem("authData");
+      return storedAuthData ? JSON.parse(storedAuthData) : null;
+    }
+    return null;
   });
 
   useEffect(() => {
-    // Guardar authData en localStorage cuando cambie
-    if (authData) {
-      localStorage.setItem("authData", JSON.stringify(authData));
-    } else {
-      localStorage.removeItem("authData");
+    // Guardar authData en localStorage cuando cambie, solo en el cliente
+    if (typeof window !== "undefined") {
+      if (authData) {
+        localStorage.setItem("authData", JSON.stringify(authData));
+      } else {
+        localStorage.removeItem("authData");
+      }
     }
   }, [authData]);
 
