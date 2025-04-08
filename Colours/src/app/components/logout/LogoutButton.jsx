6a8489c52e62ec55d5dced3a077d6542d4069f1e
PHRCOOK <pabloelleproso@gmail.com>
2025-04-07 21:11:40 -0300

    modificacion en el contexto

--- Colours/src/app/components/logout/LogoutButton.jsx
+++ Colours/src/app/components/logout/LogoutButton.jsx
@@ -1,14 +1,15 @@
 "use client";
 import { useContext } from "react";
-import { useRouter } from "next/navigation"; // Importa useRouter
+import { useRouter } from "next/navigation";
 import { AuthContext } from "../../context/AuthContext";
 
 export function LogoutButton() {
   const { setAuthData } = useContext(AuthContext);
-  const router = useRouter(); // Inicializa el router
+  const router = useRouter();
 
   const handleLogout = () => {
     setAuthData(null); // Borra el estado de la sesión
+    localStorage.removeItem("authData"); // Borra los datos del localStorage
     router.push("/login"); // Redirige a la ruta /login
   };
 
--- Colours/src/app/context/AuthContext.jsx
+++ Colours/src/app/context/AuthContext.jsx
@@ -1,10 +1,23 @@
 "use client";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [authData, setAuthData] = useState(null); // Estado para guardar la sesión
+  const [authData, setAuthData] = useState(() => {
+    // Cargar el estado inicial desde localStorage
+    const storedAuthData = localStorage.getItem("authData");
+    return storedAuthData ? JSON.parse(storedAuthData) : null;
+  });
+
+  useEffect(() => {
+    // Guardar authData en localStorage cuando cambie
+    if (authData) {
+      localStorage.setItem("authData", JSON.stringify(authData));
+    } else {
+      localStorage.removeItem("authData");
+    }
+  }, [authData]);
 
   return (
     <AuthContext.Provider value={{ authData, setAuthData }}>
