902239a4de45204f4a45cdb982c67a2cdaea9774
PHRCOOK <pabloelleproso@gmail.com>
2025-04-07 22:00:02 -0300

    correcion de bugs register

--- Colours/src/app/components/register/register.jsx
+++ Colours/src/app/components/register/register.jsx
@@ -8,7 +8,7 @@ import TermsAndConditions from "./TermsAndCondition";
 export default function Register() {
   return (
     <div className="min-h-screen flex items-center justify-center  px-4">
-      <div className="bg-gray-800/100 backdrop-blur-sm rounded-2xl p-6 md:p-10 w-full max-w-lg md:max-w-2xl lg:max-w-3xl  ">
+      <div className="backdrop-blur-sm rounded-2xl p-6 md:p-10 w-full max-w-lg md:max-w-2xl lg:max-w-3xl  ">
         <h1 className="text-white text-2xl md:text-3xl font-bold text-center mb-6">
           Registro
         </h1>
