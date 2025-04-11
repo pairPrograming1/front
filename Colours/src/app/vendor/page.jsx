aa816a2a1114619278fd9a7baf3a1ab4b06fdc3c
PHRCOOK <pabloelleproso@gmail.com>
2025-04-11 18:02:02 -0300

    correcion de bugs

--- Colours/src/app/vendor/page.jsx
+++ Colours/src/app/vendor/page.jsx
@@ -3,8 +3,8 @@ import Vendor from "../components/vendor/vendor";
 
 export default function VendorPage() {
   return (
-    <main className="flex-1 flex flex-col items-center justify-center p-4">
-      <div className="w-full max-w-4xl space-y-4 z-10">
+    <main className="flex-1 flex flex-col items-center  justify-center p-4">
+      <div className="w-full max-w-6xl bg-[#1E2330]/70 space-y-4 z-10">
         <Vendor />
       </div>
     </main>
