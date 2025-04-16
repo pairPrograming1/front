4a9889862505ec26f01549e9aada51a9b85bec1c
PHRCOOK <pabloelleproso@gmail.com>
2025-04-16 12:39:12 -0300

    componente wellcome para usuarios comunes hecho

---
+++ Colours/src/app/wellcome/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Vendor from "../components/wellcome/wellcome";
+
+export default function VendorPage() {
+  return (
+    <main className="min-h-screen w-full flex items-center justify-center bg-[#12151f]/40 p-4">
+      <div className="w-full max-w-6xl bg-[#1E2330]/70 space-y-4 z-10 p-6 rounded-xl shadow-lg">
+        <Vendor />
+      </div>
+    </main>
+  );
+}
