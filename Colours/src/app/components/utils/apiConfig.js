2509291221439cdd24f6d9130c2297eb213b9341
estebanaleart <esteban.aleart@gmail.com>
2025-06-11 16:11:35 -0300

    develop agregado en apiUrl

--- Colours/src/app/components/utils/apiConfig.js
+++ Colours/src/app/components/utils/apiConfig.js
@@ -1,10 +1,11 @@
 const apiUrls = {
   local: "http://localhost:4000",
   production: "https://backend-production-687d.up.railway.app",
+  develop:"https://backend-production-d353.up.railway.app"
 };
 
 // Exporta ambas URLs y una por defecto (cambia manualmente aquí)
 export const API_URL_LOCAL = apiUrls.local;
 export const API_URL_PRODUCTION = apiUrls.production;
 
-export default apiUrls.production; // ← Cambia a .local si necesitas desarrollo
+export default apiUrls.develop; // ← Cambia a .local si necesitas desarrollo
