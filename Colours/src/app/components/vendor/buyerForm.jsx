70ac141bbd5db4dcfe65ef160265d009685ea557
estebanaleart <esteban.aleart@gmail.com>
2025-07-13 17:34:14 +0300

    users error

--- Colours/src/app/components/vendor/buyerForm.jsx
+++ Colours/src/app/components/vendor/buyerForm.jsx
@@ -12,7 +12,7 @@ export default function BuyerInfoForm({ eventIdFromParams }) {
     dni: "",
     whatsapp: "",
     email: "",
-  })
+  })    
 
   const [eventId, setEventId] = useState(eventIdFromParams || null)
 
