7d29f406c0055afed9fb82d36e6010919c79ea07
Pablito471 <pablohernanbrito@gmail.com>
2025-07-24 23:04:20 -0300

    correcion de nombre en inicio de pagina y agregado de logo xevent

--- Colours/src/app/components/login/Login.jsx
+++ Colours/src/app/components/login/Login.jsx
@@ -1,24 +1,31 @@
 "use client";
 
 import LoginForm from "./LoginForm";
+import Image from "next/image";
 
 export default function Login() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-[#12151f]/40 px-4 py-12">
       <div className="w-full max-w-md bg-[#1E2330]/70 p-6 rounded-xl shadow-lg">
         <div className="mb-8">
-          <h1 className="text-[#FFFFFF] text-3xl font-bold">Bienvenido</h1>
-          <h2 className="text-[#FFFFFF] text-3xl font-bold mb-4">
-            a ColourApp
-          </h2>
-          <p className="text-[#EDEEF0] text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            bibendum ultricies dui, laoreet commodo nunc dictum nec.
-          </p>
+          <div className="text-center">
+            <h1 className="text-[#FFFFFF] text-3xl font-bold">Bienvenido</h1>
+            <h2 className="text-[#FFFFFF] text-3xl font-bold mb-4">
+              a XeventApp
+            </h2>
+          </div>
+          <div className="flex justify-center my-4">
+            <Image
+              src="https://res.cloudinary.com/dmjusy7sn/image/upload/v1753239784/Group_118_i3hj6p.png"
+              alt="ColourApp Logo"
+              width={150}
+              height={100}
+              className="object-contain"
+            />
+          </div>
+          <p className="text-[#EDEEF0] text-sm"></p>
         </div>
         <LoginForm />
-
-        
       </div>
     </div>
   );
