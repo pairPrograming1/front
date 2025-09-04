320bc638896b8246f1ad70129de8daf9120aba79
Pablito471 <pablohernanbrito@gmail.com>
2025-09-03 22:44:27 -0300

    modificacion en landing de login

--- Colours/src/app/components/login/OAuthButton.jsx
+++ Colours/src/app/components/login/OAuthButton.jsx
@@ -174,7 +174,9 @@ export default function OAuthButton() {
 
   return (
     <div className="text-center mt-4">
-      <p className="text-[#EDEEF0] text-sm mb-2">Continuar con Google</p>
+      <p className="text-[#EDEEF0] text-sm mb-2">
+        Registrate e Inicia Sesion con Google
+      </p>
       <button
         className="transparent flex items-center justify-center mx-auto border-0 hover:opacity-80 transition-opacity cursor-pointer"
         type="button"
